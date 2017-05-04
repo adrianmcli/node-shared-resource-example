123c9a62d8170b9260f0a166bcd0e19f049db6d3
Adrian Li <li.adrianmc@gmail.com>
2017-05-04 02:33:32 -0700

    Update feature.js

--- feature.js
+++ feature.js
@@ -1,9 +1,9 @@
+// Simply require the instantiated db object
 const db = require("./db");
 
+// Check to see what's already inside it
 console.log("[1] feature.js: " + db.get());
 
+// Add another item and inspect its contents
 db.insert("bar_from_feature.js");
-
 console.log("[2] feature.js: " + db.get());
-
-module.exports = "baz";
