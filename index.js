5d2e0e64b3077482233b69cb4245c79d74ecb9f7
Adrian Li <li.adrianmc@gmail.com>
2017-05-04 02:31:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+// Simply require the instantiated db object
 const db = require('./db')
 
+// Check to see that it is fresh
 console.log('[1] index.js: ' + db.get())
 
+// Insert something inside and check to see that it was successful
 db.insert('foo_from_index.js')
-
 console.log('[2] index.js: ' + db.get())
 
+// Require the code from feature.js
 const feature = require('./feature')
 
+// Check the state again to see that it contains code inserted by both index.js and feature.js
 console.log('[3] index.js: ' + db.get())
